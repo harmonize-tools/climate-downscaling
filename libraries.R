c6272dfb31d74e38c044fd1841b63047d417d956
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-10 13:55:30 +0200

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -1,27 +1,7 @@
-# install required packages (if they are not yet installed) and load them:
-install.and.load <- function(packagename){
-  if (!require(packagename)){
-    install.packages(packagename)
-    library(packagename)
-  } else {
-    library(packagename)
-  }
-}
-install.and.load('startR')
-install.and.load('s2dv')
-install.and.load('CSTools')
-install.and.load('easyVerification')
-install.and.load('multiApply')
-install.and.load('ClimProjDiags')
-install.and.load('plyr')
-install.and.load('nnet')
-install.and.load('FNN')
-install.and.load('ncdf4')
-install.and.load('ecmwfr')
-install.and.load('devtools')
-install.and.load('lubridate')
+if(!require('pacman')){install.packages('pacman')}
+pacman::p_load(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
 
-# source public code (not available as a package):
+# source public code (not yet available as a package):
 source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Analogs.R")
 source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Interpolation.R")
 source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intbc.R")
@@ -29,5 +9,4 @@ source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intlr.R")
 source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/LogisticReg.R")
 source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Utils.R")
 
-source('https://earth.bsc.es/gitlab/es/csdownscale/-/raw/dev-vignette/vignette/download_seasonal_data.R')
-
+#source('https://earth.bsc.es/gitlab/es/csdownscale/-/raw/dev-vignette/vignette/download_seasonal_data.R')
