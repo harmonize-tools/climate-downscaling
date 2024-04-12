579bc5e545f91a5e78f7044d8f9921eeb6511e0a
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-12 10:16:55 +0200

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,1 +1,1 @@
-install.packages(c('startR', 's2dv', 'CSTools', 'easyVerification', 'multiApply', 'ClimProjDiags', 'plyr', 'nnet', 'FNN', 'ncdf4', 'ecmwfr', 'devtools', 'lubridate')
+install.packages(c('startR', 's2dv', 'CSTools', 'easyVerification', 'multiApply', 'ClimProjDiags', 'plyr', 'nnet', 'FNN', 'ncdf4', 'ecmwfr', 'devtools', 'lubridate'))
