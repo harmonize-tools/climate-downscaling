a5962be813e79053c0206cec3f638d3284f534d9
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-13 19:56:55 +0200

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -1,12 +1,15 @@
-if(!require('pacman')){install.packages('pacman')}
-pacman::p_load(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
+# Binder:
+  install.packages("./extra_libs/ncdf4_1.22.tar.gz",repos=NULL, type="source")
+  library(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
 
-# source public code (not yet available as a package):
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Analogs.R")
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Interpolation.R")
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intbc.R")
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intlr.R")
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/LogisticReg.R")
-source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Utils.R")
+# Local: 
+  if(!require('pacman')){install.packages('pacman')}
+  pacman::p_load(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
 
-#source('https://earth.bsc.es/gitlab/es/csdownscale/-/raw/dev-vignette/vignette/download_seasonal_data.R')
+# source public code CSDownscale package (run either in Binder or Local):
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Analogs.R")
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Interpolation.R")
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intbc.R")
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intlr.R")
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/LogisticReg.R")
+  source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Utils.R")
