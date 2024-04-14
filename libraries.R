d50d57a6dba80e2d9801911873476db7d8117e34
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-14 19:18:12 +0200

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -1,10 +1,10 @@
 # Binder:
   install.packages("./extra_libs/ncdf4_1.22.tar.gz",repos=NULL, type="source")
-  library(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate, parallel)
+  library(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
 
 # Local: 
   if(!require('pacman')){install.packages('pacman')}
-  pacman::p_load(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate, parallel)
+  pacman::p_load(startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ncdf4, ecmwfr, devtools, lubridate)
 
 # source public code CSDownscale package (run either in Binder or Local):
   source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Analogs.R")
