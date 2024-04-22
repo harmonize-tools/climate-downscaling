441a8bffd649909c19d644d01cc3f4ba5aebccdb
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-22 19:16:50 +0200

    Create libraries.R

---
+++ binder/libraries.R
@@ -0,0 +1,9 @@
+install.packages("./extra_libs/ncdf4_1.22.tar.gz",repos=NULL, type="source")
+pacman::p_load(ncdf4, startR, s2dv, CSTools, easyVerification, multiApply, ClimProjDiags, plyr, nnet, FNN, ecmwfr, devtools, lubridate)
+
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Analogs.R")
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Interpolation.R")
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intbc.R")
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Intlr.R")
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/LogisticReg.R")
+source("https://earth.bsc.es/gitlab/es/csdownscale/-/raw/master/R/Utils.R")
