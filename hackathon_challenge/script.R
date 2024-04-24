3b91c6318c04214d8ebf499a492479d554f70e06
Alba-LB <129278822+Alba-LB@users.noreply.github.com>
2024-04-24 15:33:46 +0200

    Update script.R

--- hackathon_challenge/script.R
+++ hackathon_challenge/script.R
@@ -1,29 +1,41 @@
+# reference period, forecast issue date and leadtimes
+reference_period <- c(2002:2015)
+forecast_issue_date <- '2024-04'
+leadtimes <- 1
 
-point_data_1 <- as.array(read.csv("./quistocoha.csv") [,3])
-point_data_2 <- as.array(read.csv("./el_paujil.csv") [,3])
-dim(point_data_1)  <- dim(point_data_2 )<- c(length(point_data_1))
-point_data <- abind (point_data_1, point_data_2, along=2)
-names(dim(point_data)) <- c("sdate", "location")
-dim(point_data) <- c(dim(point_data), dat = 1, time = 1, var = 1 ) 
-#target_grid <- '/esarchive/recon/ecmwf/era5/monthly_mean/tas_f1h/tas_200002.nc'
-lonmin <- -90
-lonmax <- -30
-latmin <- -15
-latmax <- 0
-#sdates <- c('20000201','20010201','20020201','20030201','20040201','20050201','20060201','20070201')
-sdates <- format(ymd("20000201") + rep(years(0:13), each=1),"%Y%m%d")
-exp <- startR::Start(dat = '/esarchive/exp/ecmwf/system5c3s/monthly_mean/$var$_f6h/$var$_$sdate$.nc',
-                     var = 'tas', time = indices(1), member = 'all', sdate = sdates,
-                     lat = values(list(latmin, latmax)), lat_reorder = Sort(decreasing = FALSE),
-                     lon = values(list(lonmin, lonmax)), lon_reorder = CircularSort(0, 360),
-                     synonims = list(var = c('var','variable'), lon = c('lon', 'longitude'),
-                     lat = c('lat', 'latitude'), member = c('member','ensemble')),
-                     return_vars = list(lat = 'dat', lon = 'dat'),
-                     num_procs = 1, retrieve = TRUE)
-points <- list(lat = c(-3, -1), lon = c(360-73.3,360-71))
+# configuration of sdate_hcst (array containing the initialisation dates of the reference period)
+sdate_hcst <- paste0(reference_period, substr(forecast_issue_date,6,7))
+
+# configuration of sdate_fcst (array containing the initialisation dates of the forecast)
+sdate_fcst <- paste0(substr(forecast_issue_date,1,4), substr(forecast_issue_date,6,7))
+
+# path where to find the sample data
+exp_path <- paste0('./sample_data/ecmwf51/$var$_$sdate$01.nc')  
+obs_path <- paste0('./sample_data/era5land/$var$_$date$.nc')
+
+exp <- startR::Start(
+  dat = exp_path,
+  var = var_name,
+  sdate = sdate_hcst,
+  ensemble = 'all',
+  time = leadtimes,
+  latitude = values(list(latmin, latmax)),
+  latitude_reorder = Sort(decreasing = T),
+  longitude = values(list(lonmin, lonmax)),
+  longitude_reorder = CircularSort(-180,180),
+  synonims = list(latitude = c('lat', 'latitude'),
+                  longitude = c('lon', 'longitude'),
+                  ensemble=c('member','ensemble','number')),
+  return_vars = list(latitude = 'dat',
+                     longitude = 'dat',
+                     time = 'sdate'),
+  retrieve = TRUE)
+
+points <- list(latitude = c(-3, -1), longitude = c(-73.3,-71))
 obs_point <- point_data
 down_points<- Intbc(exp = exp, obs = obs_point, exp_lats = attr(exp, "Variables")$dat1$lat,
-                          exp_lons = attr(exp, "Variables")$dat1$lon, obs_lats = points$lat,
-                          obs_lons = points$lon, points = points, method_point_interp = 'nearest',
-                          source_file_exp = attr(exp, "Files")[1,1,1], source_file_obs = NULL,
-                          target_grid = target_grid, bc_method = 'bias', ncores = 4)
+                    exp_lons = attr(exp, "Variables")$dat1$lon, obs_lats = points$lat,
+                    obs_lons = points$lon, points = points, method_point_interp = 'nearest',
+                    source_file_exp = attr(exp, "Files")[1,1,1], source_file_obs = NULL,
+                    target_grid = 'r4000x4000', 
+                    bc_method = 'bias', ncores = 4, lon_dim = 'longitude', lat_dim = 'latitude', member_dim = 'ensemble')
